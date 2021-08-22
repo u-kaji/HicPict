f6e142913e9a319a7b43dd6d55caff18d420fcc7
u-kaji <kurinodechawonomou@gmail.com>
2021-08-22 14:43:57 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'picts/index'
   root to: "picts#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :destroy]
 end
