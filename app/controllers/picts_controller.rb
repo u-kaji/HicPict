b3db562b24a85b22e84b5f513cd5c518f1dbbe62
u-kaji <kurinodechawonomou@gmail.com>
2021-09-01 11:00:37 +0900

    検索機能追加実装

--- app/controllers/picts_controller.rb
+++ app/controllers/picts_controller.rb
@@ -3,10 +3,9 @@ class PictsController < ApplicationController
   before_action :set_pict, only:[:show, :edit, :update, :destroy]
 
   def index
-    @picts = Pict.all.order(id:"desc")
     @q = Pict.ransack(params[:q])
-    @titles = @q.result(distinct: true)
-    @comments = @q.result(distinct: true)
+    @picts = @q.result(distinct: true).order(id:"desc")
+
   end
 
   def new
