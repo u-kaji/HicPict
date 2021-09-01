1197d122850c344d76bfea71481714a3f1536e16
u-kaji <kurinodechawonomou@gmail.com>
2021-09-01 13:12:26 +0900

    ログアウト時のエラー改善

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'picts/index'
   root to: "picts#index"
+  
   resources   :users, only: [:edit, :update, :destroy]
   resources   :picts do
     resources :hics, only: [:new, :create] do
