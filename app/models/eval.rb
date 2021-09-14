329f06bb83fda6d9d259147a0bce03e8502ba661
u-kaji <kurinodechawonomou@gmail.com>
2021-09-14 16:14:30 +0900

    バリデーション入力＋rspec導入

--- app/models/eval.rb
+++ app/models/eval.rb
@@ -1,4 +1,14 @@
 class Eval < ApplicationRecord
+
+  with_options presence:true do
+    validates :review, length: {maximum:50}
+    validates :scene
+    validates :kigo
+    validates :rhythm
+    validates :tech
+    validates :original
+  end
+
   belongs_to :user
   belongs_to :hic
   has_one :pict, through: :hic
