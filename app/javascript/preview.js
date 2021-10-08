2379122cc42669afc56bc3f4878e3580e53fdf26
u-kaji <kurinodechawonomou@gmail.com>
2021-10-08 14:55:11 +0900

    JavaScript側でリサイズ試し

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
   if ( document.getElementById('item-image')){
     const ImageList = document.getElementById('image-list');
+    const THUMBNAIL_MAX_WIDTH = 500;
+    const THUMBNAIL_MAX_HEIGHT = 500;
 
     //選択した画像を表示する関数
     const createImageHTML = (blob) => {
