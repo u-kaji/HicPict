7d93689d3acb18514b8bd5abe68c38a02520ff2c
u-kaji <kurinodechawonomou@gmail.com>
2021-10-08 14:34:14 +0900

    upload定義　サイズ指定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,16 +22,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process scale: [200, 200]
+  process resize_to_fit: [200, 200]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+    version :upload do
+      process resize_to_fit: [200, 200]
+    end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
